0fbdb5a4bef580d6a29353f12ee88be04c6ca8dc
dimi1309 <dim1309@gmail.com>
2022-03-27 15:30:32 +0300

    Add function CalcDistance

--- sandbox/src/main/java/ru/homework2/Point.java
+++ sandbox/src/main/java/ru/homework2/Point.java
@@ -7,4 +7,8 @@ public class Point {
     public double x;
     public double y;
 
+    public static double distance(Point p1, Point p2) {
+        int degree = 2;
+        return Math.sqrt(Math.pow(p2.x - p1.x, degree) + (Math.pow(p2.y - p1.y, degree)));
+    }
 }
