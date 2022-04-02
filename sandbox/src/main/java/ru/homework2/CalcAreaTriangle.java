a6d8818316b9dae61a22dd987b4f2255be5ed16d
dimi1309 <dim1309@gmail.com>
2022-04-02 17:42:30 +0300

    Error correction

--- sandbox/src/main/java/ru/homework2/CalcAreaTriangle.java
+++ sandbox/src/main/java/ru/homework2/CalcAreaTriangle.java
@@ -2,21 +2,22 @@ package ru.homework2;
 
 import java.text.DecimalFormat;
 
+/**
+ * Класс проводит расчёт площади произвольного треугольника по формуле Герона по известным длинам сторон
+ * используется класс DecimalFormat для округления дробной части
+ */
 public class CalcAreaTriangle {
 
-    /**
-     * Класс проводит расчёт площади произвольного треугольника по формуле Герона по известным длинам сторон
-     * используется класс DecimalFormat для округления дробной части
-     */
     public static void main(String[] args) {
         Segments s = new Segments(1, 12, 9);
 
         DecimalFormat decimalFormat = new DecimalFormat("#.###");
-        String result = decimalFormat.format(Segments.areatriangle(s.a, s.b, s.c));
-        if ((s.a + s.b <= s.c) || (s.a + s.c <= s.b) || (s.b + s.c <= s.a))
+        String result = decimalFormat.format(Segments.areaTriangle(s.a, s.b, s.c));
+        if ((s.a + s.b <= s.c) || (s.a + s.c <= s.b) || (s.b + s.c <= s.a)) {
             System.out.println("Треугольников с такими сторонами не бывает. Одна сторона больше суммы двух других сторон.");
-        else
+        } else {
             System.out.println("Площадь треугольника со сторонами " + s.a + ", " + s.b + ", " + s.c + " равна " + result);
+        }
 
     }
 }
