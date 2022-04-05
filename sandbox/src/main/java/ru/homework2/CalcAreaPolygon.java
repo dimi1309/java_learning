7fd56c2ad3dc8843e7150980e1f9ac1f4c37d763
dimi1309 <dim1309@gmail.com>
2022-04-05 09:34:50 +0300

    Correction code

--- sandbox/src/main/java/ru/homework2/CalcAreaPolygon.java
+++ sandbox/src/main/java/ru/homework2/CalcAreaPolygon.java
@@ -3,7 +3,6 @@ package ru.homework2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 /**
  * Класс проводит расчёт площади правильного многоугольника по известному количеству
